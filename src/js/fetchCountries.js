daf3124c48e9e51cee133bc801e9e724dc682124
Igor <Travis789@gmail.com>
2021-03-09 18:22:50 +0100

    rejected promise from promise

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -3,14 +3,14 @@ const BASE_URL = 'https://restcountries.eu/rest/v2/name/';
 function fetchCountries (name) {
     return fetch(`${BASE_URL}${name}`)
     .then(response => {
-        return new Promise((resolve, reject) => {
-            if (!response.ok) {
-                reject();
-            } else {
-                resolve(response.json());
-            }
-        })
-        // return response.json();
+        // return new Promise((resolve, reject) => {
+        //     if (!response.ok) {
+        //         reject();
+        //     } else {
+        //         resolve(response.json());
+        //     }
+        // })
+        return response.json();
     });
 }
 
